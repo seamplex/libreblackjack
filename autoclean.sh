39924afb11adc5d917a6f6b1e37c32c0b74de3cf
gtheler <jeremy@seamplex.com>
2020-12-10 16:35:20 -0300

    have make dist work

--- autoclean.sh
+++ autoclean.sh
@@ -16,6 +16,8 @@ cd doc
 cat .gitignore | grep -v nbproject | sed '/^#.*/ d' | sed '/^\s*$/ d' | sed 's/^/rm -rf /' | bash
 cd ..
 
-cd po
-cat .gitignore | grep -v nbproject | sed '/^#.*/ d' | sed '/^\s*$/ d' | sed 's/^/rm -rf /' | bash
-cd ..
+rm -f players/05-no-bust/fifo players/08-mimic-the-dealer/d2p players/08-mimic-the-dealer/p2d players/30-ace-five/f
+
+# cd po
+# cat .gitignore | grep -v nbproject | sed '/^#.*/ d' | sed '/^\s*$/ d' | sed 's/^/rm -rf /' | bash
+# cd ..
